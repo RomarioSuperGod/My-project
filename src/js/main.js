792401c23f2829a329c9190f512188ce072744a6
Romario <hyperromario@yahoo.com>
2017-03-22 18:34:51 +0200

    i hope alomoust

--- src/js/main.js
+++ src/js/main.js
@@ -31,60 +31,92 @@ $(document).ready(function () {
         wrapAround: true
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         var height = $(window).scrollTop();
 
-        if(height  > 0) {
+        if (height > 0) {
             $(".home-page-header").addClass("header");
         }
-        else{
+        else {
             $(".home-page-header").removeClass("header");
         }
     });
 
     $('#form').validate({ // initialize the plugin
-            rules: {
-                inputName: {
-                    required: true,
-                    minlength: 5
-                },
-                inputEmail: {
-                    required: true,
-                    email: true
-                },
-                inputTitle: {
-                    required: true,
-                    minlength: 5
-                },
-                textArea: {
-                    required: true,
-                    minlength: 10
-                },
-                postArea: {
-                    required: true
-                }
+        rules: {
+            inputName: {
+                required: true,
+                minlength: 5
             },
-        submitHandler: function() {
+            inputEmail: {
+                required: true,
+                email: true
+            },
+            inputTitle: {
+                required: true,
+                minlength: 5
+            },
+            textArea: {
+                required: true,
+                minlength: 10
+            },
+            postArea: {
+                required: true
+            }
+        },
+        submitHandler: function () {
             toastr.success('Your information are accepted');
             $('.form').get(0).reset();
         }
     });
 
-    $('#dataTable').DataTable( {
+    $('#dataTable').DataTable({
         "ajax": "/json/dataTable.json",
 
         "columns": [
-            { "data": "Name" },
-            { "data": "Date" },
-            { "data": "Email" },
-            { "data": "City" },
-            { "data": "Personal Number" }
+            {"data": "Name"},
+            {"data": "Date"},
+            {"data": "Email"},
+            {"data": "City"},
+            {"data": "Personal Number"}
         ]
-    } );
+    });
 
-    $('#sidr!').sidr({
-        name: 'sidr-right',
-        side: 'right'
+
+    $('#simple-menu').sidr();
+    $(window).resize(function () {
+        if ($(window).width() <= 750) {
+            showMobileMenu();
+            $("#simple-menu").css("display", "block");
+        }
+        else if ($(window).width() >= 750) {
+            $("#sidr").removeClass("sidr left");
+            $("#simple-menu").css("display", "none");
+        }
+    });
+    //check screen width
+    if ($(window).width() <= 750) {
+        showMobileMenu();
+        $("#simple-menu").css("display", "block");
+    } else {
+        $("#sidr").removeClass("sidr left");
+        $("#simple-menu").css("display", "none");
+    }
+
+
+    function showMobileMenu() {
+        $('#simple-menu').sidr({
+            side: "right"
+        });
+    }
+
+    //google maps
+    $('.googleMap').click(function () {
+        $('.googleMap iframe').css("pointer-events", "auto");
+    });
+
+    $(".googleMap").mouseleave(function () {
+        $('.googleMap iframe').css("pointer-events", "none");
     });
 
 });
